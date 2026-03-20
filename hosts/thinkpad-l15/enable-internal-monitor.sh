d7d1471c7ef9c4adf50213d3672a338d64014e2d
juan_ma7u7 <jmlafuente97@gmail.com>
2026-03-20 12:52:03 -0600

    monitor scripts

---
+++ hosts/thinkpad-l15/enable-internal-monitor.sh
@@ -0,0 +1,2 @@
+hyprctl keyword monitor eDP-1,preferred,auto,1
+hyprctl keyword monitor HDMI-A-1,highrr,auto,1
---
+++ hosts/thinkpad-l15/use-only-ext-monitor.sh
@@ -0,0 +1,4 @@
+
+
+hyprctl keyword monitor eDP-1,disable
+hyprctl keyword monitor HDMI-A-1,highrr,auto,1
