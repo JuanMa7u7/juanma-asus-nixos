199715a57eb416a34ea769a6f88c6ed0d3d5d9f3
juan_ma7u7 <jmlafuente97@gmail.com>
2026-04-09 16:59:43 -0600

    hyprlock custom config files

--- hosts/mamalona/hm/confs/default.nix
+++ hosts/mamalona/hm/confs/default.nix
@@ -1,4 +1,6 @@
 { ... }:
 {
   home.file.".config/hypr/hyprland/monitors.conf".source = ./caelestia/hypr/hyprland/monitors.conf;
+  home.file.".config/hypr/hyprlock/greetd-wallbash.conf".source = ./caelestia/hypr/hyprlock/greetd-wallbash.conf;
+  home.file.".config/hypr/hyprlock/theme.conf".source = ./caelestia/hypr/hyprlock/theme.conf;
 }
