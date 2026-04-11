bb17c37d15721c8275f86b823b8d63f0e695614c
Juan Manuel Lafuente Araiza <jmlafuente97@gmail.com>
2026-04-10 18:22:09 -0600

    Wallpapers syslink

--- hosts/thinkpad-l15/hm/confs/default.nix
+++ hosts/thinkpad-l15/hm/confs/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   home.file.".config/hypr/hyprland/monitors.conf".source = ./caelestia/hypr/hyprland/monitors.conf;
+  home.file."Pictures/Wallpapers".source = ../../../../common/hm/confs/caelestia/wallpapers;
   home.file.".config/hypr/hyprlock/greetd-wallbash.conf".source = ./caelestia/hypr/hyprlock/greetd-wallbash.conf;
   home.file.".config/hypr/hyprlock/theme.conf".source = ./caelestia/hypr/hyprlock/theme.conf;
 }
