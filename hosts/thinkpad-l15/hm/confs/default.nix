8d55a8431d10a4e9cd2bc5ca072cd88117f95db7
juan_ma7u7 <jmlafuente97@gmail.com>
2026-04-14 10:46:59 -0600

    thinkpad gifs

--- hosts/thinkpad-l15/hm/confs/default.nix
+++ hosts/thinkpad-l15/hm/confs/default.nix
@@ -2,6 +2,5 @@
 {
   home.file.".config/hypr/hyprland/monitors.conf".source = ./caelestia/hypr/hyprland/monitors.conf;
   home.file."Pictures/Wallpapers".source = ../../../../common/hm/confs/caelestia/wallpapers;
-  home.file.".config/hypr/hyprlock/greetd-wallbash.conf".source = ./caelestia/hypr/hyprlock/greetd-wallbash.conf;
-  home.file.".config/hypr/hyprlock/theme.conf".source = ./caelestia/hypr/hyprlock/theme.conf;
+  home.file."Pictures/Caelestia".source = ./caelestia/gifs;
 }
