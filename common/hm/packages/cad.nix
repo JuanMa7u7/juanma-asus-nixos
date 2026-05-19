101481f6e6e9fad12cf5bf2c14fefd1766327b38
JuanMa7u7 <jmlafuente97@gmail.com>
2026-05-19 12:46:36 -0600

    fix

--- common/hm/packages/cad.nix
+++ common/hm/packages/cad.nix
@@ -3,6 +3,5 @@
   home.packages = with pkgs; [
     blender
     cura-appimage
-    snapmaker-luban
   ];
 }
--- common/hm/programs/pencil.nix
+++ common/hm/programs/pencil.nix
@@ -6,7 +6,7 @@ let
 
   src = pkgs.fetchurl {
     url = "https://www.pencil.dev/download/Pencil-linux-x86_64.AppImage";
-    hash = "sha256-pu7KAEUQh3k/tcvy58P3u6L3wUf/wY2PxV6VhKS+AC0=";
+    hash = "sha256-nuf4jVPU5wsR1MwFXr0llAOGxQ4vwiQNEoiBwPwbAXQ=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
