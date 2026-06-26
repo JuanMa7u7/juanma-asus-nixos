59b7ab96b7966ccd1792174e4b059ca2b2896fcb
JuanMa7u7 <jmlafuente97@gmail.com>
2026-06-26 17:39:44 -0600

    fix

--- common/hm/programs/pencil.nix
+++ common/hm/programs/pencil.nix
@@ -6,7 +6,7 @@ let
 
   src = pkgs.fetchurl {
     url = "https://www.pencil.dev/download/Pencil-linux-x86_64.AppImage";
-    hash = "sha256-/MqoYsPMkpBYhsKzRQabzvDAUxX3M6ZbRrZBhSKvkW8=";
+    hash = "sha256-0i/bdO+E/iMnZa/GXmLTS4C+RYk0Z/MJPE+A8lkYCE8=";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
